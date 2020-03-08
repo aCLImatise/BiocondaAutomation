52ac5547212c83a0cdc6faa8cbfc9ef4c6076d6d
Michael Milton <ttmigueltt@gmail.com>
2020-03-09 10:39:34 +1100

    Update

--- bioconda_cli/main.py
+++ bioconda_cli/main.py
@@ -6,12 +6,14 @@ import os
 import pathlib
 import subprocess
 
+
 def get_conda_binaries():
-    conda_bin = os.environ.get('CONDA_EXE')
-    if conda_bin is None:
+    conda_env = os.environ.get('CONDA_PREFIX')
+    if conda_env is None:
         raise Exception('You must be in a conda environment to run this')
 
-    return set(pathlib.Path(conda_bin).parent.iterdir())
+    return set((pathlib.Path(conda_env) / 'bin').iterdir())
+
 
 @click.group()
 def main():
@@ -19,8 +21,18 @@ def main():
 
 
 @main.command()
+def list_bin():
+    print('\n'.join([str(x) for x in get_conda_binaries()]))
+
+
+@main.command()
 def env_dump():
-    stdout, stderr, retcode = run_command('search', 'bwa', '-c', 'bioconda', '--json')
+    stdout, stderr, retcode = run_command(
+        'search',
+        '--override-channels',  # Don't use system default channels
+        '--channel', 'bioconda',  # Only use bioconda
+        '--json'  # We need JSON so we can parse it
+    )
     packages = list(json.loads(stdout).keys())
     print(yaml.dump({
         'name': 'all_bioconda',
@@ -31,7 +43,8 @@ def env_dump():
 
 @main.command(help='Store all the "--help" outputs in the provided directory')
 @click.argument('out', type=click.Path(file_okay=False, dir_okay=True, exists=True))
-@click.argument('environment', type=click.Path(file_okay=True, dir_okay=False, exists=True))
+@click.argument('environment',
+                type=click.Path(file_okay=True, dir_okay=False, exists=True))
 def list_help(out, environment):
     initial_bin = get_conda_binaries()
     run_command('install', '--file', str(environment))
@@ -39,7 +52,23 @@ def list_help(out, environment):
 
     # Output the new binaries installed
     for bin in final_bin - initial_bin:
-        if resubprocess.run([bin, '--help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
+        proc = subprocess.run(
+            [bin, '--help'],
+            check=False,
+            capture_output=True
+        )
+        out_file = (out / bin)
+
+        with out_file.with_suffix('.stdout.txt').open('w'):
+            out_file.write(proc.stdout)
+        with out_file.with_suffix('.stderr.txt').open('w'):
+            out_file.write(proc.stderr)
+
+@main.command(help='Convert all the help outputs into CWL')
+def acclimatise(dir):
+    dir = pathlib.Path(dir)
+    for file in dir.iterdir():
+        output = file.with_suffix('.cwl')
 
 if __name__ == '__main__':
     main()
