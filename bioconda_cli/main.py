fec72fad8207d063a38ee05434f1274fb0e713ea
Michael Milton <ttmigueltt@gmail.com>
2020-03-30 19:37:34 +1100

    Dump to stdout

--- bioconda_cli/main.py
+++ bioconda_cli/main.py
@@ -5,6 +5,7 @@ import os
 import pathlib
 from acclimatise import best_cmd
 from acclimatise.yaml import yaml
+import sys
 
 
 def get_conda_binaries():
@@ -34,11 +35,11 @@ def env_dump():
         '--json'  # We need JSON so we can parse it
     )
     packages = list(json.loads(stdout).keys())
-    print(yaml.dump({
+    yaml.dump({
         'name': 'all_bioconda',
         'channels': ['bioconda'],
         'dependencies': packages
-    }))
+    }), sys.stdout
 
 
 @main.command(help='Store all the "--help" outputs in the provided directory')
