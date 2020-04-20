8b2b1e81f735c39b83a035120c0a42f4285c51da
Michael Milton <ttmigueltt@gmail.com>
2020-04-20 18:33:03 +1000

    Pre-commit hooks, logging improvements

--- bioconda_cli/main.py
+++ bioconda_cli/main.py
@@ -1,51 +1,88 @@
-import click
-from conda.cli.python_api import run_command
+import io
 import json
 import os
 import pathlib
+import sys
+from contextlib import contextmanager, redirect_stderr, redirect_stdout
+from itertools import chain
+
+from conda.cli.python_api import run_command
+
+import click
 from acclimatise import explore_command
 from acclimatise.yaml import yaml
-import sys
+
+
+def ctx_print(ctx, msg):
+    if ctx["VERBOSE"]:
+        print(msg)
+
+
+@contextmanager
+def log_around(msg: str, ctx: dict = {}):
+    """
+    Wraps a long function invocation with a message like:
+    "Running long process... done"
+    """
+    # Skip this unless we're in verbose mode
+    if not ctx.get("VERBOSE"):
+        return
+
+    # Store the stdout and stderr to avoid clogging up the logs
+    err = io.StringIO()
+    out = io.StringIO()
+    print(msg + "...")
+    with redirect_stderr(err), redirect_stdout(out):
+        yield
+    print("Done.")
+
+    # Indent the stdout/stderr
+    for line in chain(out.readlines(), err.readlines()):
+        print("\t" + line)
 
 
 def get_conda_binaries():
-    conda_env = os.environ.get('CONDA_PREFIX')
+    conda_env = os.environ.get("CONDA_PREFIX")
     if conda_env is None:
-        raise Exception('You must be in a conda environment to run this')
+        raise Exception("You must be in a conda environment to run this")
 
-    print('Conda env is {}'.format(conda_env))
-    return set((pathlib.Path(conda_env) / 'bin').iterdir())
+    print("Conda env is {}".format(conda_env))
+    return set((pathlib.Path(conda_env) / "bin").iterdir())
 
 
 @click.group()
-def main():
-    pass
+@click.option("--verbose", is_flag=True)
+@click.pass_context
+def main(ctx, verbose):
+    ctx["VERBOSE"] = verbose
 
 
 @main.command()
 def list_bin():
-    print('\n'.join([str(x) for x in get_conda_binaries()]))
+    print("\n".join([str(x) for x in get_conda_binaries()]))
 
 
 @main.command()
-@click.option('--test', is_flag=True)
-def env_dump(test=False):
+@click.option("--test", is_flag=True)
+@click.pass_context
+def env_dump(ctx, test=False):
     if test:
         packages = [
             # 'samtools',
-            'bwa',
+            "bwa",
             # 'pisces'
         ]
     else:
         stdout, stderr, retcode = run_command(
-            'search',
-            '--override-channels',  # Don't use system default channels
-            '--channel', 'bioconda',  # Only use bioconda
-            '--json'  # We need JSON so we can parse it
+            "search",
+            "--override-channels",  # Don't use system default channels
+            "--channel",
+            "bioconda",  # Only use bioconda
+            "--json",  # We need JSON so we can parse it
         )
         packages = json.loads(stdout).keys()
 
-    sys.stdout.writelines([package + '\n' for package in packages])
+    sys.stdout.writelines([package + "\n" for package in packages])
     # yaml.dump({
     #     'name': 'all_bioconda',
     #     'channels': ['bioconda'],
@@ -54,28 +91,29 @@ def env_dump(test=False):
 
 
 @main.command(help='Store all the "--help" outputs in the provided directory')
-@click.argument('out', type=click.Path(file_okay=False, dir_okay=True, exists=True))
-@click.argument('environment',
-                type=click.Path(file_okay=True, dir_okay=False, exists=True))
-def acclimatise(out, environment):
-    initial_bin = get_conda_binaries()
-    run_command(
-        'install',
-        '--channel', 'bioconda',
-        '--file', str(environment)
-    )
+@click.argument("out", type=click.Path(file_okay=False, dir_okay=True, exists=True))
+@click.argument(
+    "environment", type=click.Path(file_okay=True, dir_okay=False, exists=True)
+)
+@click.pass_context
+def acclimatise(ctx, out, environment):
+    with log_around("Listing conda packages"):
+        initial_bin = get_conda_binaries()
+
+    with log_around("Installing conda packages"):
+        run_command("install", "--channel", "bioconda", "--file", str(environment))
     final_bin = get_conda_binaries()
 
     # Output the help text to the directory
     for bin in final_bin - initial_bin:
-        print('Exploring {}'.format(bin))
-        try:
-            cmd = explore_command([str(bin)])
-            with (pathlib.Path(out) / bin.name).with_suffix('.yml').open('w') as fp:
-                yaml.dump(cmd, fp)
-        except Exception as e:
-            print('Command {} failed with error {} using the output'.format(bin, e))
+        with log_around("Exploring {}".format(bin)):
+            try:
+                cmd = explore_command([str(bin)])
+                with (pathlib.Path(out) / bin.name).with_suffix(".yml").open("w") as fp:
+                    yaml.dump(cmd, fp)
+            except Exception as e:
+                print("Command {} failed with error {} using the output".format(bin, e))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='bioconda_find_cli',
+    name="bioconda_find_cli",
     packages=find_packages(),
-    version='0.0.1',
-    install_requires=[
-        'click',
-        'acclimatise>=0.0.7',
-        'ruamel.yaml==0.16.5',
-    ],
+    version="0.0.1",
+    install_requires=["click", "acclimatise>=0.0.7", "ruamel.yaml==0.16.5",],
+    extras_require={"dev": ["pytest", "pre-commit"],},
     entry_points={
         "console_scripts": [
-            'find_cli = bioconda_cli.main:main',
+            "find_cli = bioconda_cli.main:main",
             # more script entry points ...
         ],
-    }
+    },
 )
