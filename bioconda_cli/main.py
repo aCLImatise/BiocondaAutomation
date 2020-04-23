2ad57e968200cbf303fe489b3a513847742bf4ca
Michael Milton <ttmigueltt@gmail.com>
2020-04-24 01:50:39 +1000

    New behaviour whereby we create envs for each package sequentially

--- bioconda_cli/main.py
+++ bioconda_cli/main.py
@@ -3,6 +3,7 @@ import json
 import os
 import pathlib
 import sys
+import tempfile
 from contextlib import contextmanager, redirect_stderr, redirect_stdout
 from itertools import chain
 
@@ -12,7 +13,7 @@ from conda.exceptions import DryRunExit
 import click
 from acclimatise import explore_command
 from acclimatise.yaml import yaml
-from packaging import version
+from packaging.version import parse
 
 
 def ctx_print(ctx, msg):
@@ -67,12 +68,19 @@ def list_bin(ctx):
     print("\n".join([str(x) for x in get_conda_binaries(ctx)]))
 
 
-@main.command(help="Lists all the packages in bioconda")
+@main.command(help="Lists all the packages in bioconda, one per line")
 @click.option(
     "--test", is_flag=True, help="Use a tiny subset of bioconda for testing purposes"
 )
+# @click.option('--versions', is_flag=True, help='Include package versions in output')
+@click.option(
+    "--last-spec",
+    type=click.Path(dir_okay=False),
+    help="Path to a previous output from this command, to "
+    "ensure we only acclimatise new tool versions",
+)
 @click.pass_context
-def list_packages(ctx, test=False):
+def list_packages(ctx, test=False, last_spec=None):
     stdout, stderr, retcode = run_command(
         "search",
         *(
@@ -86,43 +94,12 @@ def list_packages(ctx, test=False):
         )
     )
 
-    # The package names are keys to the output dict
-    for key in json.loads(stdout).keys():
-        print(key)
-
-
-@main.command(
-    help="Produces a file containing all the (system-compatible) versions of all the bioconda packages, "
-    "excluding those that haven't changed and don't need upgrading"
-)
-@click.argument("package_file", type=click.Path(dir_okay=False, exists=True))
-@click.option(
-    "--last-spec",
-    type=click.Path(dir_okay=False),
-    help="Path to a previous output from this command, to "
-    "ensure we only acclimatise new tool versions",
-)
-@click.pass_context
-def list_versions(ctx, package_file, last_spec=None):
-    try:
-        stdout, stderr, retcode = run_command(
-            "install",
-            "--channel",
-            "bioconda",
-            "--file",
-            str(package_file),
-            "--json",
-            "--dry-run",
-            use_exception_handler=True,
-        )
-    except DryRunExit as e:
-        stdout = e.stdout
-
-    # Get a set of packages at their latest compatible versions in bioconda
     packages = set()
-    installs = json.loads(stdout)["actions"]["LINK"]
-    for package in installs:
-        packages.add("{}={}".format(package["name"], package["version"]))
+
+    # The package names are keys to the output dict
+    for key, versions in json.loads(stdout).items():
+        latest_version = max(versions, key=lambda v: parse(v["version"]))
+        packages.add("{}={}".format(key, latest_version["version"]))
 
     # The previous spec file basically defines a set of versions *not* to use
     if last_spec is not None:
@@ -135,43 +112,122 @@ def list_versions(ctx, package_file, last_spec=None):
     sys.stdout.writelines([package + "\n" for package in packages - last_spec_versions])
 
 
+# @main.command(
+#     help="Produces a file containing all the (system-compatible) versions of all the bioconda packages, "
+#     "excluding those that haven't changed and don't need upgrading"
+# )
+# @click.argument("package_file", type=click.Path(dir_okay=False, exists=True))
+# @click.option(
+#     "--last-spec",
+#     type=click.Path(dir_okay=False),
+#     help="Path to a previous output from this command, to "
+#     "ensure we only acclimatise new tool versions",
+# )
+# @click.pass_context
+# def list_versions(ctx, package_file, last_spec=None):
+#     try:
+#         stdout, stderr, retcode = run_command(
+#             "install",
+#             "--channel",
+#             "bioconda",
+#             "--file",
+#             str(package_file),
+#             "--json",
+#             "--dry-run",
+#             use_exception_handler=True,
+#         )
+#     except DryRunExit as e:
+#         stdout = e.stdout
+#
+#     # Get a set of packages at their latest compatible versions in bioconda
+#     packages = set()
+#     installs = json.loads(stdout)["actions"]["LINK"]
+#     for package in installs:
+#         packages.add("{}={}".format(package["name"], package["version"]))
+#
+#     # The previous spec file basically defines a set of versions *not* to use
+#     if last_spec is not None:
+#         with open(last_spec) as fp:
+#             last_spec_versions = set((line.strip() for line in fp.readlines()))
+#     else:
+#         last_spec_versions = set()
+#
+#     # Subtract the two sets to produce the final result
+#     sys.stdout.writelines([package + "\n" for package in packages - last_spec_versions])
+
+
 @main.command(help="Install a list of packages and list the new binaries")
-@click.argument("spec", type=click.Path(dir_okay=False))
-@click.pass_context
-def install(ctx, spec):
-    initial_bin = get_conda_binaries(ctx)
-
-    with log_around("Installing conda packages", ctx.obj):
-        run_command("install", "--channel", "bioconda", "--file", str(spec))
-
-    final_bin = get_conda_binaries(ctx)
-
-    for new_bin in final_bin - initial_bin:
-        print(str(new_bin))
-
-
-@main.command(help='Store all the "--help" outputs in the provided directory')
-@click.argument("bins", type=click.Path(file_okay=True, dir_okay=False, exists=True))
+# A file which contains one package per line
+@click.argument("packages", type=click.Path(dir_okay=False))
 @click.argument("out", type=click.Path(file_okay=False, dir_okay=True, exists=True))
 @click.pass_context
-def acclimatise(ctx, out, bins):
-    with open(bins) as bins_fp:
-        # Output the help text to the directory
-        for line in bins_fp:
-            exe = pathlib.Path(line.strip())
-            with log_around("Exploring {}".format(exe), ctx.obj):
-                try:
-                    cmd = explore_command([str(exe)])
-                    with (pathlib.Path(out) / exe.name).with_suffix(".yml").open(
-                        "w"
-                    ) as fp:
-                        yaml.dump(cmd, fp)
-                except Exception as e:
-                    print(
-                        "Command {} failed with error {} using the output".format(
-                            exe, e
-                        )
+def install(ctx, packages, out):
+    # Ignore all the default conda packages
+    initial_bin = get_conda_binaries(ctx)
+
+    # Iterate each package in the input file
+    with open(packages) as fp:
+        for line in fp:
+            versioned_package = line.strip()
+            package, version = versioned_package.split("=")
+
+            # We have to install and uninstall each package separately because doing it all at once forces Conda to
+            # solve an environment with thousands of packages in it, which runs forever (I tried for several days)
+            with log_around("Installing {}".format(package), ctx.obj):
+                with tempfile.TemporaryDirectory() as dir:
+                    run_command(
+                        "create",
+                        "--yes",
+                        "-p",
+                        dir,
+                        "--channel",
+                        "bioconda",
+                        "--channel",
+                        "conda-forge",
+                        versioned_package,
                     )
+                    run_command("activate", dir)
+                    new_bin = get_conda_binaries(ctx)
+
+                    # Acclimatise each new executable
+                    for exe in new_bin - initial_bin:
+                        with log_around("Exploring {}".format(exe), ctx.obj):
+                            try:
+                                cmd = explore_command([str(exe)])
+                                with (pathlib.Path(out) / exe.name).with_suffix(
+                                    ".yml"
+                                ).open("w") as out_fp:
+                                    yaml.dump(cmd, out_fp)
+                            except Exception as e:
+                                print(
+                                    "Command {} failed with error {} using the output".format(
+                                        exe, e
+                                    )
+                                )
+
+
+# @main.command(help='Store all the "--help" outputs in the provided directory')
+# @click.argument("bins", type=click.Path(file_okay=True, dir_okay=False, exists=True))
+# @click.argument("out", type=click.Path(file_okay=False, dir_okay=True, exists=True))
+# @click.pass_context
+# def acclimatise(ctx, out, bins):
+#     with open(bins) as bins_fp:
+#         # Output the help text to the directory
+#         for line in bins_fp:
+#             exe = pathlib.Path(line.strip())
+#             with log_around("Exploring {}".format(exe), ctx.obj):
+#                 try:
+#                     cmd = explore_command([str(exe)])
+#                     with (pathlib.Path(out) / exe.name).with_suffix(".yml").open(
+#                         "w"
+#                     ) as fp:
+#                         yaml.dump(cmd, fp)
+#                 except Exception as e:
+#                     print(
+#                         "Command {} failed with error {} using the output".format(
+#                             exe, e
+#                         )
+#                     )
 
 
 if __name__ == "__main__":
