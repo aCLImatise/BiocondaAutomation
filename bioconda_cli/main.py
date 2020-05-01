4aba46b0513bf83a61aaa7ef82a857b8c37f9676
Michael Milton <ttmigueltt@gmail.com>
2020-05-01 17:48:02 +1000

    Remove the tool repository builds, add the "wrappers" command

--- bioconda_cli/main.py
+++ bioconda_cli/main.py
@@ -11,14 +11,15 @@ from functools import partial
 from itertools import chain
 from logging import ERROR, getLogger
 from multiprocessing import Lock, Pool
-from typing import List, Tuple
+from typing import List, Optional, Tuple
+
+from conda.api import Solver
+from conda.cli.python_api import run_command
+from conda.exceptions import UnsatisfiableError
 
 import click
 from acclimatise import Command, CwlGenerator, WdlGenerator, explore_command
 from acclimatise.yaml import yaml
-from conda.api import Solver
-from conda.cli.python_api import run_command
-from conda.exceptions import UnsatisfiableError
 from packaging.version import parse
 
 # Yes, it's a global: https://stackoverflow.com/a/28268238/2148718
@@ -26,6 +27,85 @@ lock = Lock()
 
 # This might make conda a bit quieter
 getLogger("conda").setLevel(ERROR)
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    kwargs = vars(args)
+    func = args.func
+    kwargs.pop("func")
+    func(**kwargs)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", action="store_true")
+    subparsers = parser.add_subparsers()
+
+    cmd_list = subparsers.add_parser(
+        "list-packages", help="Lists all the packages in bioconda, one per line"
+    )
+    cmd_list.add_argument(
+        "--test",
+        action="store_true",
+        help="Use a tiny subset of bioconda for testing purposes",
+    )
+    cmd_list.add_argument(
+        "--filter-r",
+        action="store_true",
+        help="Filter out R packages, which don't tend to have CLIs",
+    )
+    cmd_list.add_argument(
+        "--last-spec",
+        type=click.Path(dir_okay=False),
+        help="Path to a previous output from this command, to "
+        "ensure we only acclimatise new tool versions",
+    )
+    cmd_list.set_defaults(func=list_packages)
+
+    cmd_install = subparsers.add_parser(
+        "install", help="Install a list of packages and list the new binaries"
+    )
+    cmd_install.add_argument(
+        "--processes",
+        "-p",
+        type=int,
+        default=None,
+        help="Use this many processes instead of all the available CPUs",
+    )
+    cmd_install.add_argument(
+        "--exit-on-failure",
+        "-x",
+        action="store_true",
+        help="Exit the entire process if any package fails",
+    )
+    cmd_install.add_argument(
+        "packages",
+        type=click.Path(dir_okay=False),
+        help="A file that has one package with "
+        "associated version number, one per line",
+    )
+    cmd_install.add_argument(
+        "out",
+        type=click.Path(file_okay=False, dir_okay=True, exists=True),
+        help="A directory into which to produce output files",
+    )
+    cmd_install.set_defaults(func=install)
+
+    cmd_wrappers = subparsers.add_parser(
+        "wrappers",
+        help="Recursively convert all .yml dumped Commands into tool wrappers",
+    )
+    cmd_wrappers.add_argument(
+        "dir", type=click.Path(dir_okay=True, file_okay=False, exists=True)
+    )
+    cmd_wrappers.add_argument(
+        "--out", "-o", type=click.Path(dir_okay=True, file_okay=False, exists=True)
+    )
+    cmd_wrappers.set_defaults(func=cmd_wrappers)
+
+    return parser
 
 
 def flush():
@@ -114,73 +194,6 @@ def activate_env(env: pathlib.Path):
 
     # Then reset those variables
     os.environ.update(env_backup)
-
-
-def main():
-    parser = get_parser()
-    args = parser.parse_args()
-    kwargs = vars(args)
-    func = args.func
-    kwargs.pop("func")
-    func(**kwargs)
-
-
-def get_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--verbose", action="store_true")
-    subparsers = parser.add_subparsers()
-
-    cmd_list = subparsers.add_parser(
-        "list-packages", help="Lists all the packages in bioconda, one per line"
-    )
-    cmd_list.add_argument(
-        "--test",
-        action="store_true",
-        help="Use a tiny subset of bioconda for testing purposes",
-    )
-    cmd_list.add_argument(
-        "--filter-r",
-        action="store_true",
-        help="Filter out R packages, which don't tend to have CLIs",
-    )
-    cmd_list.add_argument(
-        "--last-spec",
-        type=click.Path(dir_okay=False),
-        help="Path to a previous output from this command, to "
-        "ensure we only acclimatise new tool versions",
-    )
-    cmd_list.set_defaults(func=list_packages)
-
-    cmd_install = subparsers.add_parser(
-        "install", help="Install a list of packages and list the new binaries"
-    )
-    cmd_install.add_argument(
-        "--processes",
-        "-p",
-        type=int,
-        default=None,
-        help="Use this many processes instead of all the available CPUs",
-    )
-    cmd_install.add_argument(
-        "--exit-on-failure",
-        "-x",
-        action="store_true",
-        help="Exit the entire process if any package fails",
-    )
-    cmd_install.add_argument(
-        "packages",
-        type=click.Path(dir_okay=False),
-        help="A file that has one package with "
-        "associated version number, one per line",
-    )
-    cmd_install.add_argument(
-        "out",
-        type=click.Path(file_okay=False, dir_okay=True, exists=True),
-        help="A directory into which to produce output files",
-    )
-    cmd_install.set_defaults(func=install)
-
-    return parser
 
 
 def list_bin(ctx):
@@ -296,14 +309,6 @@ def commands_from_package(
                             ) as out_fp:
                                 yaml.dump(cmd, out_fp)
 
-                            # Dump a WDL version of the tool
-                            wdl = WdlGenerator().generate_wrapper(cmd)
-                            (out_subdir / exe.name).with_suffix(".wdl").write_text(wdl)
-
-                            # Dump a CWL version of the tool
-                            cwl = CwlGenerator().generate_wrapper(cmd)
-                            (out_subdir / exe.name).with_suffix(".cwl").write_text(cwl)
-
                         except Exception as e:
                             if exit_on_failure:
                                 raise e
@@ -321,6 +326,32 @@ def commands_from_package(
                                     verbose,
                                 )
     flush()
+
+
+def generate_wrappers(
+    command_dir: os.PathLike, output_dir: Optional[os.PathLike] = None
+):
+    """
+    Recursively convert all .yml dumped Commands into tool wrappers
+    :param command_dir: Directory to convert from
+    :param output_dir: If provided, output files in the same directory structure, but in this directory
+    """
+    for definition in pathlib.Path(command_dir).rglob("*.yml"):
+        with definition.open() as fp:
+            cmd = yaml.load(fp)
+
+        if output_dir:
+            stem = pathlib.Path(output_dir) / definition.relative_to(command_dir)
+        else:
+            stem = definition
+
+        # Dump a WDL version of the tool
+        wdl = WdlGenerator().generate_wrapper(cmd)
+        stem.with_suffix(".wdl").write_text(wdl)
+
+        # Dump a CWL version of the tool
+        cwl = CwlGenerator().generate_wrapper(cmd)
+        stem.with_suffix(".cwl").write_text(cwl)
 
 
 def install(packages, out, verbose=False, processes=None, exit_on_failure=False):
